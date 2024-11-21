03b5305e6a5886d55f7abd0ec4d3b7b229da95e0
Nemanja Milicevic <the.nemanja.milicevic@gmail.com>
2024-11-21 15:06:49 +0100

    Autoclose kiwi store

--- src/main/java/kiwi/Kiwi.java
+++ src/main/java/kiwi/Kiwi.java
@@ -6,69 +6,68 @@ import kiwi.storage.KiwiStore;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
 import java.util.Optional;
 
 public class Kiwi {
 
-    public static void main(String[] args) {
-        KiwiStore store = KiwiStore.open(Paths.get("db").normalize());
-
+    public static void main(String[] args) throws Exception {
         System.out.println("\n-------");
         System.out.println("KiwiDB");
         System.out.println("-------\n");
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(" ", 3);
-                String command = parts[0].toUpperCase();
+        try (KiwiStore store = KiwiStore.open()) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(" ", 3);
+                    String command = parts[0].toUpperCase();
 
-                switch (command) {
-                    case "PUT":
-                        if (parts.length != 3) {
-                            System.out.println("Usage: PUT key value");
-                            continue;
-                        }
-                        store.put(Bytes.wrap(parts[1]), Bytes.wrap(parts[2]));
-                        break;
-
-                    case "GET":
-                        if (parts.length != 2) {
-                            System.out.println("Usage: GET key");
-                            continue;
-                        }
-                        Optional<Bytes> maybeValue = store.get(Bytes.wrap(parts[1]));
-                        if (maybeValue.isEmpty()) {
-                            System.out.println("Key not found");
-                            continue;
-                        } else {
-                            System.out.println(maybeValue.get());
+                    switch (command) {
+                        case "PUT":
+                            if (parts.length != 3) {
+                                System.out.println("Usage: PUT key value");
+                                continue;
+                            }
+                            store.put(Bytes.wrap(parts[1]), Bytes.wrap(parts[2]));
                             break;
-                        }
 
-                    case "DEL":
-                        if (parts.length != 2) {
-                            System.out.println("Usage: DEL key");
-                            continue;
-                        }
-                        store.delete(Bytes.wrap(parts[1]));
-                        break;
+                        case "GET":
+                            if (parts.length != 2) {
+                                System.out.println("Usage: GET key");
+                                continue;
+                            }
+                            Optional<Bytes> maybeValue = store.get(Bytes.wrap(parts[1]));
+                            if (maybeValue.isEmpty()) {
+                                System.out.println("Key not found");
+                                continue;
+                            } else {
+                                System.out.println(maybeValue.get());
+                                break;
+                            }
 
-                    case "SIZE":
-                        System.out.println(store.size());
-                        break;
+                        case "DEL":
+                            if (parts.length != 2) {
+                                System.out.println("Usage: DEL key");
+                                continue;
+                            }
+                            store.delete(Bytes.wrap(parts[1]));
+                            break;
 
-                    case "EXIT":
-                        return;
+                        case "SIZE":
+                            System.out.println(store.size());
+                            break;
 
-                    default:
-                        System.out.println("Unknown command. Available commands: PUT, GET, DEL, EXIT");
-                        break;
+                        case "EXIT":
+                            return;
+
+                        default:
+                            System.out.println("Unknown command. Available commands: PUT, GET, DEL, EXIT");
+                            break;
+                    }
                 }
+            } catch (IOException ex) {
+                System.err.println("Failed to read input: " + ex.getMessage());
             }
-        } catch (IOException ex) {
-            System.err.println("Failed to read input: " + ex.getMessage());
         }
     }
 }
